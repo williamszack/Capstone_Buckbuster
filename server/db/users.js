4219baf2bcc7e56378ede86a1c855231c488c690
Evansville Rojas <evansville185@gmail.com>
2023-04-14 20:06:54 -0700

    Completed creating SQL queries for users.js file - Feature - Waiting for Review ER 4/4/23

--- server/db/users.js
+++ server/db/users.js
@@ -6,55 +6,102 @@ const bcrypt = require("bcrypt");
 const SALT_COUNT = 10;
 
 async function createUser({ name, email, username, password, admin }) {
-    // need to add NOT NULL for name, email, password in table
-    //do not show/offer admin select on FE register screen, autmatically set to default
-    const hashedPassword = await bcrypt.hash(password, SALT_COUNT);
-    const create = { name, email, username, hashedPassword, admin }
-    try {
-        const {rows: [user], } = await client.query(
-        `
+	//do not show/offer admin select on FE register screen, autmatically set to default
+	const hashedPassword = await bcrypt.hash(password, SALT_COUNT);
+	const create = { name, email, username, hashedPassword, admin };
+	try {
+		const {
+			rows: [user],
+		} = await client.query(
+			`
         INSERT INTO users(name, email, username, password )
         VALUES ($1, $2, $3, $4, $4)
         ON CONFLICT (username) DO NOTHING
         RETURNING *;
         `,
-            [create.name, create.email, create.username, create.hashedPassword, create.admin]
-
-        );
-        delete user.password;
-
-        return user;
-    } catch (error) {
-        console.error("Error creating user:", error);
-    }
+			[create.name, create.email, create.username, create.hashedPassword, create.admin]
+		);
+		delete user.password;
+		return user;
+	} catch (error) {
+		console.error("Error creating user:", error);
+	}
 }
 
 async function getUserByUsername(userName) {
-    try {
-        const { rows: [user], } = client.query(
-        `
+	try {
+		const {
+			rows: [user],
+		} = client.query(
+			`
         SELECT *
         FROM users
         WHERE username = $1;
         `,
-            [userName]
-        );
-
-        return user;
-    } catch (error) {
-        console.log(`Error finding user by ${userName}:`, error);
-    }
+			[userName]
+		);
+		if (!user) {
+			console.log(`${userName} does not exist`);
+			return null;
+		} else {
+			return user;
+		}
+	} catch (error) {
+		console.log("Error finding user:", error);
+	}
 }
 
 async function getUser({ username, password }) {
-    if (!username || !password) return;
+	if (!username || !password) {
+		console.log("Invalid credentials");
+		return;
+	}
 
-    try {
-        const user = await getUserByUsername(username);
-        const hashedPassword = user.password;
+	try {
+		const user = await getUserByUsername(username);
+		const hashedPassword = user.password;
+		const passwordMatch = await bcrypt.compare(password, hashedPassword);
+		if (!passwordMatch) {
+			console.log("Invalid credentials");
+			return null;
+		} else {
+			delete user.password;
+		}
 
-
-    } catch (error) {
-        console.error(`Error finding user ${username}:`, error);
-    }
+		return user;
+	} catch (error) {
+		console.error("Error finding user:", error);
+	}
 }
+
+async function getUserById(userId) {
+	try {
+		const {
+			rows: [user],
+		} = await client.query(
+			`
+        SELECT *
+        FROM users
+        WHERE id = $1;
+        `,
+			[user]
+		);
+		if (!user) {
+			console.log(`${userId} does not exist`);
+			return null;
+		} else {
+			delete user.password;
+		}
+
+		return user;
+	} catch (error) {
+		console.log("Error finding id:", error);
+	}
+}
+
+module.exports = {
+	createUser,
+	getUser,
+	getUserByUsername,
+	getUserById,
+};
