a53be86f05deee2f2557d03eea4237dafc93f618
Evansville Rojas <evansville185@gmail.com>
2023-04-20 16:52:24 -0700

    products api enpoint functions - quality - InProgress ER 4/20/2023

--- server/api/products.js
+++ server/api/products.js
@@ -1,31 +1,37 @@
-const express = require('express');
+const express = require("express");
 const productsRouter = express.Router();
 const { requiredUser } = require("./utils");
-const { getAllProducts } = require("../db")
+const { getAllProducts } = require("../db");
 
 /*-------api/products/health-------*/
-productsRouter.get('/health', async (req, res,) => {
-    res.send({
-        message: "products endpoint is working"
-      });
+productsRouter.get("/health", async (req, res) => {
+	res.send({
+		message: "products endpoint is working",
+	});
 });
 
 // adding reviews api to products
 
 //GET api/products
-productsRouter.get('/', async (req, res, next) => {
-    try {
-    const products = await getAllProducts();
+productsRouter.get("/", async (req, res, next) => {
+	try {
+		const products = await getAllProducts();
 
-    res.send(
-      products
-    );
-    } catch (error) {
-        next({error})
-    }
-  });
+		res.send(products);
+	} catch (error) {
+		next({ error });
+	}
+});
 
-//GET api/products/:productid/reviews
+//GET api/products/:product_id/reviews
+productsRouter.get("/:product_id/reviews", async (req, res, next) => {
+	const { product_id } = req.params;
+
+	try {
+	} catch (error) {
+		next(error);
+	}
+});
 
 //GET api/products/:productid
 
@@ -38,4 +44,4 @@ productsRouter.get('/', async (req, res, next) => {
 //DELTE api/products/:productid
 //admin only
 
-module.exports = productsRouter
+module.exports = productsRouter;
