876eec3c0d167af9a1321ee63ddb0c8128af82e8
Evansville Rojas <evansville185@gmail.com>
2023-04-21 13:44:24 -0700

    in progress ER 4/21/2023

--- server/api/products.js
+++ server/api/products.js
@@ -12,6 +12,17 @@ productsRouter.get("/health", async (req, res) => {
 
 // adding reviews api to products
 
+// Middleware function to check if user is an admin
+function isAdmin(req, res, next) {
+	if (req.user && req.user.admin) {
+		// If user exist and is an admin, continue to the next middleware/route handler
+		next();
+	} else {
+		// User is not an admin, so send a 403 Forbidden response
+		res.sendStatus(403);
+	}
+}
+
 //GET api/products
 productsRouter.get("/", async (req, res, next) => {
 	try {
@@ -37,6 +48,15 @@ productsRouter.get("/:product_id/reviews", async (req, res, next) => {
 
 //POST api/products
 // page to add new products-admin only
+productsRouter.post("/", isAdmin, async (req, res, next) => {
+	const { name, description, price, genre, quantity, image_url } = req.body;
+
+	try {
+		// Your code to create a new product
+	} catch (error) {
+		next(error);
+	}
+});
 
 //PATCH api/products/:productid
 //admin only
