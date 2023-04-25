1b71c05b6335e1ab572f5fbf436ec75e3d689ef6
Bradley Jenkins <bradjenkins84@gmail.com>
2023-04-24 19:49:15 -0500

    first api/cart

--- server/api/cart.js
+++ server/api/cart.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const cartRouter = express.Router();
 const { requiredUser } = require("./utils");
-const { getCartItemsByUserId } = require("../db")
+const { getCartItemsByUserId, deleteCartItem, addToCart } = require("../db");
+
 
 
 /*-------api/cart/health-------*/
@@ -16,8 +17,10 @@ cartRouter.get('/health', async (req, res,) => {
 cartRouter.get('/:user_id', async (req, res, next) => {
   const { user_id } = req.params;
 try {
+
 const cartItems = await getCartItemsByUserId(user_id)
 res.send(cartItems)
+
 }catch (error) {
   next(error);
 }
@@ -25,7 +28,33 @@ res.send(cartItems)
 })
 //user must be logged in
 
-//PATCH api/cart/
+
+//DELETE api/cart/:user_id/:product_id
+cartRouter.delete('/:user_id/:product_id', async (req, res, next) => {
+  const { user_id , product_id } = req.params;
+try {
+
+const DeleteItem = await deleteCartItem(user_id, product_id)
+res.send(DeleteItem)
+
+}catch (error) {
+  next(error);
+}
+
+})
+
 //user must be logged in
+//POST api/cart
+
+cartRouter.post('/', async (req, res, next) => {
+  const { user_id , product_id } =req.body;
+  try {
+    const newItem = await addToCart({user_id, product_id})
+    res.send(newItem)
+
+  }catch (error){
+    next(error);
+  }
+})
 
 module.exports = cartRouter
