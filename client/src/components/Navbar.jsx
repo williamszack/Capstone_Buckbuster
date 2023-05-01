c27d65d543b4ea6a7815be086604ea82a44080d3
williamszack <williamszck@gmail.com>
2023-05-01 19:00:25 -0400

    first navbar commit

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -5,8 +5,22 @@ import React from 'react'
 import '../css/Navbar.css'
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <div>
+      <nav className="navbar">
+        <div className="logo">
+          <img src="logo.png" alt="Logo" />
+          <h1>Buckbuster</h1>
+        </div>
+        <div className="links">
+          <a href="..">Login</a>
+          <button>Register</button>
+          <a href="..">Cart</a>
+          <a href="..">Home</a>
+          <a href="..">Profile</a>
+        </div>
+      </nav>
+    </div>
+  );
+};
 
 export default Navbar
