1530d0001bcdc7ef51850e3bfffe7cfb45896d3c
Kenton <700brennan@gmail.com>
2023-05-01 18:14:51 -0500

    final navbar merge

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 //persistent--not it's own route
 //child of App.js
-
+import image from '../pictures/bb-donkey.png'
 import React from 'react'
 import '../css/Navbar.css'
 const Navbar = () => {
@@ -8,12 +8,12 @@ const Navbar = () => {
     <div>
       <nav className="navbar">
         <div className="logo">
-          <img src="logo.png" alt="Logo" />
-          <h1>Buckbuster</h1>
+          <img src={image} alt="Logo"/>
+          <h1 className='title'>Buckbuster</h1>
         </div>
         <div className="links">
           <a href="..">Login</a>
-          <button>Register</button>
+          <a href="..">Register</a>
           <a href="..">Cart</a>
           <a href="..">Home</a>
           <a href="..">Profile</a>
