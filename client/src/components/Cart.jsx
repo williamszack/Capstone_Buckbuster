4e5206b770e2a8df9c731977171e61b20e598513
Kenton <700brennan@gmail.com>
2023-05-03 20:17:54 -0500

    shows when user is logged in

--- client/src/components/Cart.jsx
+++ client/src/components/Cart.jsx
@@ -3,7 +3,6 @@ import React, { useState, useEffect } from "react";
 import { getUsersCart } from "../api/cart";
 import '../css/Cart.css'
 
-
 const Cart = () => {
 const [cartItems, setCartItems] =useState([])
 
@@ -15,7 +14,6 @@ const fetchUserCart = async()=>{
 }
  fetchUserCart()
   },[])
-
 
   return (
   <div className='shoppingPage--container'>
@@ -41,63 +39,4 @@ const fetchUserCart = async()=>{
   )
 }
 
-// cartItems =
-
-// [
-
-//   item = { 
-//     id: 2
-//     name: "The godfather",
-//     price: "12.99" 
-//   },
-
-//   item = {
-//     id: 3
-//     name: "Batman",
-//     price: "9.99"
-//   }
-
-// ]
-
 export default Cart
-
-{/* <div>
-<h2>Your Cart</h2>
- {cartItems.map((item) => ( */}
-//      <div key={item.id}>
-//        <p>{item.name}</p>
-//        <p>{item.price}</p>
-//      </div>
-//    ))}
-//  </div>
-
-
-// function Cart() {
-//   const [cartItems, setCartItems] = useState([]);
-
-//   useEffect(() => {
-//     // API call to fetch cart by user_id
-//     fetch(`https://example.com/api/cart/${user_id}`)
-//       .then((response) => response.json())
-//       .then((data) => {
-//         setCartItems(data.items);
-//       })
-//       .catch((error) => {
-//         console.log(error);
-//       });
-//   }, [user_id]);
-
-  // return (
-  //   <div>
-  //     <h2>Your Cart</h2>
-  //     {cartItems.map((item) => (
-  //       <div key={item.id}>
-  //         <p>{item.name}</p>
-  //         <p>{item.price}</p>
-  //       </div>
-  //     ))}
-  //   </div>
-  // );
-// }
-
-
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,12 +1,22 @@
 //persistent--not it's own route
 //child of App.js
 import image from '../pictures/bb-donkey.png'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import '../css/Navbar.css'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 
 
 const Navbar = () => {
+
+  const isAuthenticated = !!localStorage.getItem("token");
+  const user = localStorage.getItem("user")
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    localStorage.clear()
+    navigate("/")
+  } 
+
   return (
     <div>
       <nav className="navbar">
@@ -15,11 +25,13 @@ const Navbar = () => {
           <h1 className='title'>Buckbuster</h1>
         </div>
         <div className="links">
-          <Link to="/login"><a href="login">Login</a></Link>
-          <Link to="/register"><a href="register">Register</a></Link>
+          {isAuthenticated && <h2 className='loggedInUser'>Welcome {user}</h2>}
+          {!isAuthenticated && <Link to="/login"><a href="login">Login</a></Link>}
+          {!isAuthenticated && <Link to="/register"><a href="register">Register</a></Link>}
           <Link to="/cart"><a href="cart">Cart</a></Link>
           <Link to="/"><a href="home">Home</a></Link>
           <a href="..">Profile</a>
+          {isAuthenticated && <a classname="logout--button" onClick={handleLogout} href="..">Logout</a>}
         </div>
       </nav>
     </div>
