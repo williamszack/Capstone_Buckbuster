a828e7987603373e390aa4407d4f9663b5b029cc
Andrew Der <andrew.g.der@gmail.com>
2023-05-08 20:05:56 -0400

    search component complete

--- client/src/components/Search.jsx
+++ client/src/components/Search.jsx
@@ -1,11 +1,41 @@
 //Child of home.jsx
 
-import React from 'react'
-import '../css/Search.css'
-const Search = () => {
-  return (
-    <div>Search</div>
-  )
-}
+import React, { useState, useEffect } from "react";
+import { getAllProducts } from "../api/home";
+import "../css/Search.css";
+const Search = (props) => {
+  const [searchInput, setSearchInput] = useState("");
+  // const [products, setProducts] = useState("");
 
-export default Search
+  const handleChange = (e) => {
+    setSearchInput(e.target.value);
+  };
+
+  const handleSearch = () => {
+    const lowercaseSearchInput = searchInput.toLowerCase(); // Convert searchInput to lowercase
+    const results = props.products.filter((product) => {
+      if (product.name.toLowerCase().includes(lowercaseSearchInput)) {
+        // Convert product name to lowercase
+        console.log("Movie title:", product.name);
+        return true;
+      }
+      return false;
+    });
+    props.setFilteredData(results);
+    setSearchInput("");
+  };
+
+  return (
+    <div>
+      <input
+        type="text"
+        placeholder="Search for movies here"
+        onChange={handleChange}
+        value={searchInput}
+      />
+      <button onClick={handleSearch}>Search</button>
+    </div>
+  );
+};
+
+export default Search;
